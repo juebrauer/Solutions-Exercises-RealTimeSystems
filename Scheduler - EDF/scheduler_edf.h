dbe82e60995dbbb59ee24e74e326fe7d263c63ff
juebrauer <juergen.brauer@hs-kempten.de>
2017-05-20 12:08:58 +0200

    Adding solution for exercise "Scheduler EDF"

---
+++ Scheduler - EDF/scheduler_edf.h
@@ -0,0 +1,36 @@
+/// File: scheduler_edf.h
+///
+/// Task: Interface of Earliest Deadline First Scheduler
+///
+/// ---
+/// by Prof. Dr. Jürgen Brauer, www.juergenbrauer.org
+
+
+#pragma once
+
+#include <iostream>
+
+#include "scheduler.h"
+
+using namespace std;
+
+/// EDF: Earliest Deadline First
+/// see https://en.wikipedia.org/wiki/Earliest_deadline_first_scheduling
+
+class scheduler_edf : public scheduler
+{
+public:
+
+            scheduler_edf(int quantum);
+
+  int       choose_next_task();
+
+
+  /// new methods
+
+  bool      schedulability_test(deque<task*> task_list);
+
+  ///
+
+};
+
