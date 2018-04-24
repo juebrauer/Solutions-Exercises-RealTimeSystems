cd7d5da04bfed804d2e680e01c3af9dfd0b1915b
juebrauer <juergen.brauer@hs-kempten.de>
2018-04-24 17:45:31 +0200

    found_counter added for checking whether binary search really works as expected for different cases (number present vs. number not present)

--- WCET/WCET.cpp
+++ WCET/WCET.cpp
@@ -7,6 +7,7 @@
 #define NR_EXPS    10000000
 
 int loop_counter;
+int found_counter;
 
 ///
 /// Binary search algorithm
@@ -40,6 +41,7 @@ int binary_search(const int A[], const int N, const int search_value)
    //printf("\nLoops needed: %d\n", loop_counter);
    if (found)
    {
+       found_counter++;
       //printf("Found the number %d in the array at location %d!\n", search_value, middle);
       return middle;
    }
@@ -57,9 +59,12 @@ int* setup_strictly_monotonous_increasing_random_array(int size)
 {
    int* A = new int[size];
 
-   A[0] = 1;
+   A[0] = 0;
    for (int i=1; i<size; i++)
-      A[i] = A[i-1] + (rand() % 5 + 5);
+      A[i] = A[i-1] + 5;
+
+   printf("\nValue of first element (nr. 0) in array is : %d", A[0]);
+   printf("\nValue of last element (nr. %d) in array is : %d\n", size-1, A[size-1]);
 
    return A;
 
@@ -84,6 +89,7 @@ int main()
    // 2. estimate average case runtime    
    printf("\n\nAverage Case Execution Runtime test:\n");
    loop_counter = 0;
+   found_counter = 0;
    tic = clock(); // returns the processor time consumed by the program
    for (int expnr=0; expnr<NR_EXPS; expnr++)
    {
@@ -93,26 +99,30 @@ int main()
    computation_time = (double)(toc - tic) / CLOCKS_PER_SEC;
    printf("Results:\n");
    printf("\t Total number of loops done        : %d\n", loop_counter);
-   printf("\t Total runtime for all experiments : %.15f sec\n", computation_time);
+   printf("\t Number found                      : %d times\n", found_counter);
+   printf("\t Total runtime for all experiments : %.15f sec\n", computation_time);   
    printf("\t Estimated average case runtime    : %.15f sec\n", computation_time / (double)NR_EXPS);
 
 
    // 3. estimate WCET
-   int value_that_is_not_in_array = A[ARRAY_SIZE - 1] + 1;
-   //int value_that_is_not_in_array = 0;
-   //int value_that_is_not_in_array = A[rand() % ARRAY_SIZE] + 1;
+   int value_that_is_not_in_array;
+   value_that_is_not_in_array = A[ARRAY_SIZE - 1] + 1;
+   //value_that_is_not_in_array = -1;
    printf("\n\nWorst Case Execution Runtime (WCET) test:\n");
+   printf("\nSearching for number %d in array\n", value_that_is_not_in_array);
    loop_counter = 0;
+   found_counter = 0;
    tic = clock();
    for (int expnr = 0; expnr<NR_EXPS; expnr++)
    {
-      //binary_search(A, ARRAY_SIZE, A[expnr]+1);
-      binary_search(A, ARRAY_SIZE, value_that_is_not_in_array);
+      binary_search(A, ARRAY_SIZE, A[expnr]+1);
+      //binary_search(A, ARRAY_SIZE, value_that_is_not_in_array);
    }
    toc = clock();
    computation_time = (double)(toc - tic) / CLOCKS_PER_SEC;
    printf("Results:\n");
    printf("\t Total number of loops done        : %d\n", loop_counter);
+   printf("\t Number found                      : %d times\n", found_counter);
    printf("\t Total runtime for all experiments : %.15f sec\n", computation_time);
    printf("\t Estimated WCET                    : %.15f sec\n", computation_time / (double)NR_EXPS);
    
