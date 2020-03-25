e9649669b67db6eec6df0b28f51b32dba11c3efc
juebrauer <juebrauer@gmail.com>
2020-03-25 15:29:57 +0100

    Added a Linux time stamp method example for showing how to measure code execution times using the time stamp method.

---
+++ linux_examples/01_time_stamp_example/time_stamp_example.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <time.h>
+#include <stdio.h>
+#include <cmath>
+
+
+double f1()
+{
+   double result = 0.0;
+   for (int i = 0; i < 1e6; i++)
+      result += cos(i);
+   return result;
+}
+
+double f2()
+{
+   double result = 0.0;
+   for (int i = 0; i < 1e7; i++)
+      result += cos(i);
+   return result;
+}
+
+double f3()
+{
+   double result = 0.0;
+   for (int i = 0; i < 1e8; i++)
+      result += cos(i);
+   return result;
+}
+
+
+int main()
+{
+
+   clock_t tic, toc;
+   double time_elapsed;
+     
+   // volatile double result = 0;
+   volatile double result = 0;
+
+   std::cout << "CLOCKS_PER_SEC=" << CLOCKS_PER_SEC << std::endl;
+   std::cout << "Press enter to continue!" << std::endl;
+   std::cin.ignore();
+
+   tic = clock();
+   result += f1();   
+   toc = clock();
+   time_elapsed = (double)(toc - tic) / CLOCKS_PER_SEC;
+   std::cout << "tic=" << tic << 
+                " toc=" << toc <<
+                " --> time elpased=" << time_elapsed <<
+                " seconds" << std::endl;
+
+   tic = clock();
+   result += f2();
+   toc = clock();
+   time_elapsed = (double)(toc - tic) / CLOCKS_PER_SEC;
+   std::cout << "tic=" << tic << 
+                " toc=" << toc <<
+                " --> time elpased=" << time_elapsed <<
+                " seconds" << std::endl;
+
+   tic = clock();
+   result += f3();
+   toc = clock();
+   time_elapsed = (double)(toc - tic) / CLOCKS_PER_SEC;
+   std::cout << "tic=" << tic << 
+                " toc=" << toc <<
+                " --> time elpased=" << time_elapsed <<
+                " seconds" << std::endl;
+
+   //printf("\nResult = %f\n", result);
+   
+} // end main 
